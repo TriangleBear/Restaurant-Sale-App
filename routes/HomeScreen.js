7f8ec33219aa6783853ae246adfd6e352317562e
Im3jay <3jayrevilla@gmail.com>
2021-11-14 10:48:46 +0800

    Update HomeScreen.js
    Food Items now have their own Order and Remove buttons, now separately contained (bgColor can be changed)

--- routes/HomeScreen.js
+++ routes/HomeScreen.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text, Button, StyleSheet, SafeAreaView, ScrollView, Image, Alert} from 'react-native';
+import {View, Text, Button, StyleSheet, SafeAreaView, ScrollView, Image, Alert, StatusBar} from 'react-native';
 
 export function Homebar(){
     return(
@@ -15,36 +15,60 @@ export function FoodMenubar(){
     return(
         <SafeAreaView style={styles.orderText}>
             <ScrollView>
+            <View style={styles.container}>
             <Text style={{fontSize: 25}}>
                 Adobo: Php 60
             </Text>
             <Image
                 source={require('./HomeScreenAssets/Pork-Adobo-3.jpg')}
             />
+            <View style={styles.orderPanel}>
             <Button
                 title="Order"
                 onPress={()=>Alert.alert('Added Adobo')}
             />
+            <Button
+                title="Remove"
+                onPress={()=>Alert.alert('Removed Adobo')}
+            />
+            </View>
+            </View>
+            <View style={styles.container}>
             <Text style={{fontSize: 25}}>
                 Chopsuey: Php 40
             </Text>
             <Image
                 source={require('./HomeScreenAssets/chop-suey.jpg')}
             />
+            <View style={styles.orderPanel}>
             <Button
                 title="Order"
-                onPress={()=>Alert.alert('Added Chopsuey')}
+                onPress={()=>Alert.alert('Added Adobo')}
             />
+            <Button
+                title="Remove"
+                onPress={()=>Alert.alert('Removed Adobo')}
+            />
+            </View>
+            </View>
+            <View style={styles.container}>
             <Text style={{fontSize: 25}}>
                 Nilagang Baka: Php 100
             </Text>
             <Image
                 source={require('./HomeScreenAssets/nilagang-baka.jpg')}
             />
+            <View style={styles.orderPanel}>
             <Button
                 title="Order"
-                onPress={()=>Alert.alert('Added Chosuey')}
+                onPress={()=>Alert.alert('Added Adobo')}
             />
+            <Button
+                title="Remove"
+                onPress={()=>Alert.alert('Removed Adobo')}
+            />
+            </View>
+            </View>
             </ScrollView>
         </SafeAreaView>
     );
@@ -75,6 +99,17 @@ export function Aboutbar(){
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 4,
+        marginBottom: StatusBar.currentHeight || 2,
+        backgroundColor: '#dcdedc' 
+    },
+    orderPanel: {
+        flexDirection: 'row',
+        justifyContent: 'start',
+        margin: 5
+    },
     homeText: {
         flex: 1,
         backgroundColor: '#3498DB',

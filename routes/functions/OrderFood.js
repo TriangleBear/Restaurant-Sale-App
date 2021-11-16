559a5d90d6a1790b8e2748a138955c4a13a6f99c
TriangleBear <kakanini31@gmail.com>
2021-11-16 15:35:26 +0800

    Moved Directory

--- routes/functions/OrderFood.js
+++ routes/functions/OrderFood.js
@@ -1,7 +1,4 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 import 'react-native';
-
-export const AddOrder=()=>{
-    
-}

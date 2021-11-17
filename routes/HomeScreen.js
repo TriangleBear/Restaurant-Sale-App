b6e3f8deee1a4f1e850d9c36054f65073d1cd822
TriangleBear <kakanini31@gmail.com>
2021-11-17 13:44:37 +0800

    Added StyleSheet

--- routes/HomeScreen.js
+++ routes/HomeScreen.js
@@ -8,7 +8,7 @@ import {
     ImageBackground,
 } from 'react-native'
 
-export default function Homebar() {
+export function Homebar() {
     return (
         <ImageBackground style={styles.homeBG} source={require('../assets/HomeScreenAssets/Home_Image.jpg')}>
             <View style={styles.container}>
@@ -20,6 +20,57 @@ export default function Homebar() {
                     style={styles.homeText}> Waiter's Tablet</Text>
             </View>
         </ImageBackground>
+    );
+}
+
+let name1 = "Designer / Front End Developer: Yvette Marionne Medrano";
+let name2 = "Back End Developer: Aj Revilla";
+let name3 = "Full Stack Developer: Kurt Jayson Zacarias";
+export function Aboutbar() {
+    return (
+        <View>
+            <View style={styles.homeTopView}>
+                <Text style={[{ color: "white" },
+                { fontSize: 30 },
+                { fontWeight: "bold" }]}>Waiter's Tablet</Text>
+                <Text style={[{ textAlign: "center" },
+                { color: "white" },
+                { fontSize: 15 },
+                { margin: 20 }]}> Our mission is to get orders from the customer and have the waiter collect the lists</Text>
+
+            </View>
+
+
+
+            <View style={styles.homeMidView}></View>
+
+            <Text style={[{ color: "black" },
+            { fontSize: 30 },
+            { fontWeight: "bold" },
+            { textAlign: "center" },
+            { margin: 20 }]}>The Developers:</Text>
+
+            <View style={{ flexDirection: "row", margin: 5 }}>
+                <Image style={styles.aboutImg} source={require('../assets/HomeScreenAssets/yvette.jpeg')} />
+
+                <Text style={styles.aboutText}>{name1}</Text>
+
+            </View>
+            <View style={{ flexDirection: "row", margin: 5 }}>
+
+                <Image style={styles.aboutImg} source={require('../assets/HomeScreenAssets/jay.jpg')} />
+
+                <Text style={styles.aboutText}>{name2}</Text>
+            </View>
+            <View style={{ flexDirection: "row", margin: 5 }}>
+
+                <Image style={styles.aboutImg} source={require('../assets/HomeScreenAssets/kurt.jpg')} />
+
+                <Text style={styles.aboutText}>{name3}</Text>
+
+            </View>
+            <Text style={{ fontSize: 5 }}>egg</Text>
+        </View>
     );
 }
 const styles = StyleSheet.create({

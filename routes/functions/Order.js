caba2ca699bd1bdafefb343211287d80cbf382ac
TriangleBear <kakanini31@gmail.com>
2021-11-19 12:16:57 +0800

    Create Order.js

---
+++ routes/functions/Order.js
@@ -0,0 +1,30 @@
+import React from 'react';
+export const Pk_Sin = "Pork Sinigang";
+export const Fh_Sin = "Fish Sinigang";
+export const Prwn_Sin = "Prawn Sinigang";
+export const Prk_Adb = "Pork Adobo";
+export const Chk_Adb = "Chicken Adobo";
+export const Tino_Mnk = "Tinolang Manok";
+export const Bula_Sup = "Bulalo Soup";
+export const Crpy_Pata = "Crispy Pata";
+export const Pnct_Cntn = "Pancit Canton";
+export const Pnct_Bhn = "Pancit Bihon";
+export const Bcl_Exp = "Bicol Express";
+export const Bstk_Tgl = "Bistik Tagalog";
+export const Rice = "Rice";
+export const Sar_15 = "Sarsi 1.5";
+export const Ryl_15 = "Royal 1.5";
+export const Ck_15 = "Coke 1.5";
+export const Sprt_15 = "Sprite 1.5";
+export const IceT = "Ice Tea";
+export const Pnaple_Juice = "Pineapple Juice";
+
+    /**const [food,setFood]=React.useState([
+    
+    { title: 'Sarsi 1.5', price: 60, key: 14},
+    { title: 'Royal 1.5', price: 60, key: 15},
+    { title: 'Coke 1.5', price: 60, key: 16},
+    { title: 'Sprite 1.5', price: 60, key: 17},
+    { title: 'Ice Tea', price: 55, key: 18},
+    { title: 'Pineapple Juice', price: 55, key: 19}
+]); */
